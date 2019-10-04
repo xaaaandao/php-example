65bd0f062cae4144ab8d35b777ff851f5f8e1ca2
Alexandre Kajihara <alexandre.ykz@gmail.com>
2019-10-03 21:18:41 -0300

    Adicionando info de psr

---
+++ psr.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * PSR1
+ * -> Usar tags <?php e <?=
+ * -> Codificar apenas em UTF-8
+ * -> Não misturar em um arquivo php inclue, echo, funções
+ * -> Nome de classes deve sempre ter as primeiras letras maiuscúlas da palavra
+ * -> Constante devem ter todas as letras maiuscúlas e separadas por underline
+ * -> Nome de métodos devem ser iguais do Java (primeira letra minúsculo da palavra e o resto maiuscúlo palavra)
+ */
+
+ /**
+  * PSR2
+  * -> Seguir o padrão PSR1
+  * -> Ùltima linha do código deve ser deixado em branco
+  * -> Fechar as tags só se misturar com o HTML
+  * -> 120 caracteres por linha
+  * -> Não inserir espaços em brancos no final da linha
+  * -> linha em branco pode
+  * -> declaração de variáveis uma por linha
+  * -> Indentação deve ser usar 4 espaços em brancos ao invés de tab
+  * -> palavras chaves (true, false, null) devem ser escritas com letras minúsculas
+  * -> após um namespace deve se ter uma linha em branco
+  * -> as declarações use devem ser feitas após o namespace
+  * -> extends e implements devem ser declaradas na mesma linha do nome da classe
+  * -> Todos métodos devem ser declarados a visibilidade (public, protected, private)
+  * -> Não por espaço entre o nome do método e os argumentos
+  * -> chave de abertura de um método deve ficar na linha de baixo (chave fechamento também)
+  * -> argumentos do método => espaço após cada vírgula
+  * -> argumentos do método => pode separar os argumentos em várias linhas
+  * -> abstract, final, static devem vir antes da visibilidade
+  * -> statis deve ir depois da visibilidade
+  * -> if, else, elseif, for, foreach, while, do while, try, catch espaço entre ele e o argumento, chave de abertura => na mesma linha e espaço entre os argumentos
+  * -> usar elseif ao invés de elseif
+  */

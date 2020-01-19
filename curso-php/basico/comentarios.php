084bd2e472e10001c1a9208720e2cd636c31d92f
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-01-19 13:26:47 -0300

    comentários

---
+++ curso-php/basico/comentarios.php
@@ -0,0 +1,29 @@
+<div class="titulo">
+    Comentários PHP
+</div>
+
+<?php
+    echo "Estou no PHP";
+    // echo "Comentário";
+    # echo "Estou no PHP";
+    # echo //"Estou no PHP";
+    // echo #"Estou no PHP";
+    // Fechamento do bloco é mais forte que o comentário
+// # ?>
+
+<p> Depois do primeiro bloco!</p>
+<!--
+    <p> Comentário HTML 1</p>
+    <p> Comentário HTML 2</p>
+    <p> Comentário HTML 3</p>
+-->
+
+<?php
+    /*
+     Linha 1 do comentário
+     Linha 2 do comentário
+     Linha 3 do comentário
+?> */
+?>
+
+<p>Depois do segundo bloco!</p>
